9b9009d16202bda0d46284566dd1c822246f263f
MaxLivesInTheMatrix <133292790+MaxLivesInTheMatrix@users.noreply.github.com>
2024-12-13 11:14:12 -0800

    Removed Contact Page
    Too many bots on the contact page, literally 0 real connections. Removing the page.

--- blog/views.py
+++ blog/views.py
@@ -17,9 +17,6 @@ def about(request):
 def resume(request):
     return render(request, 'blog/resume.html', {'title' : 'Hire me!'} )
 
-def contact(request):
-    return render(request, 'blog/contact.html', {'title' : 'Contact Information'} )
-
 def arduino(request):
     return render(request, 'blog/arduino.html', {'title' : 'Digital Clutch Slipper'} )
 def webdev(request):
@@ -30,27 +27,3 @@ def rambo(request):
 
 def triggerbot(request):
     return render(request, 'blog/triggerbot.html', {'title' : 'Overwatch 2 Trigger Bot'})
-
-def send_email(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        subject = request.POST.get('subject')
-        message = request.POST.get('message')
-
-        # Customize the email content as needed
-        email_content = f"From: {name}\nEmail: {email}\nSubject: {subject}\nMessage: {message}"
-
-        # Replace with your email configuration
-        send_mail(
-            'Subject of the email',
-            email_content,
-            email,  # Sender's email
-            ['maxsorinengineering@gmail.com'],  # List of recipient(s)
-            fail_silently=False,
-        )
-
-        # Redirect after sending the email
-       # return HttpResponseRedirect(reverse('http://localhost:8000/about/'))  # Replace with your success page URL
-   # else:
-        return render(request, 'blog/about.html')  # Replace 'contact.html' with your actual template
